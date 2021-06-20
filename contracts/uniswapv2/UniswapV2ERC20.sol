2c8adc86b6d43f72e6574dbaf9f503cfe62e2b7b
dome <dome@tel.co.th>
2021-06-20 22:51:27 +0700

    Update UniswapV2ERC20.sol

--- contracts/uniswapv2/UniswapV2ERC20.sol
+++ contracts/uniswapv2/UniswapV2ERC20.sol
@@ -7,7 +7,7 @@ import './libraries/SafeMath.sol';
 contract UniswapV2ERC20 {
     using SafeMathUniswap for uint;
 
-    string public constant name = 'BamSwap LP Token';
+    string public constant name = 'DfySwap LP Token';
     string public constant symbol = 'BLP';
     uint8 public constant decimals = 18;
     uint  public totalSupply;
